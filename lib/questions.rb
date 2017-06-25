200df10e0e13e436f548359c4122525d2eb7d817
Panteha <panteha.ahmadi@gmail.com>
2017-06-25 21:02:01 +0100

    convert_array_to_a_hash

--- lib/questions.rb
+++ lib/questions.rb
@@ -141,6 +141,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  Hash[array.each_slice(2).to_a]
 end
 
 # get all the letters used in an array of words and return

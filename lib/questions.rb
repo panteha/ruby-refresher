778689999e9da4c73dde86cbfaa219f3364850b8
Panteha <panteha.ahmadi@gmail.com>
2017-06-25 20:18:24 +0100

    get_first_half_of_string

--- lib/questions.rb
+++ lib/questions.rb
@@ -60,6 +60,7 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string[0, (string.size / 2.0).round ]
 
 end
 
